f4311e1093cc3974606b20329282ed57dfcdaa13
Alexander Jhosue Toapanta Jama <141873111+AlexanderToapanta@users.noreply.github.com>
2025-05-06 17:39:57 -0500

    Componentes hasta modal, con diseño

--- Pagina_Personal/script.js
+++ Pagina_Personal/script.js
@@ -1,3 +1,13 @@
 function mostrarAlerta(id) {
     document.getElementById(id).classList.remove('d-none');
   }
+
+ document.addEventListener("DOMContentLoaded", function () {
+  const btnGuardar = document.getElementById("btnGuardar");
+
+  btnGuardar.addEventListener("click", function () {
+    alert("Funciona correctamente.");
+  });
+});
+
+  
